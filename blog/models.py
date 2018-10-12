8324008fa50f0fab7a9cdd1acb44b040b2c41039
hiwoshishei <1374462869@qq.com>
2018-10-12 17:53:15 +0800

    blog_second

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.encoding import python_2_unicode_compatible
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField(auto_now=True)
+    #摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+    # on_delete
+    # on_delete = None,  # 删除关联表中的数据时,当前表与其关联的field的行为
+    # on_delete = models.CASCADE,  # 删除关联数据,与之关联也删除
+    # on_delete = models.DO_NOTHING,  # 删除关联数据,什么也不做
+    # on_delete = models.PROTECT,  # 删除关联数据,引发错误ProtectedError
+    # # models.ForeignKey('关联表', on_delete=models.SET_NULL, blank=True, null=True)
+    # on_delete = models.SET_NULL,  # 删除关联数据,与之关联的值设置为null（前提FK字段需要设置为可空,一对一同理）
+    # # models.ForeignKey('关联表', on_delete=models.SET_DEFAULT, default='默认值')
+    # on_delete = models.SET_DEFAULT,  # 删除关联数据,与之关联的值设置为默认值（前提FK字段需要设置默认值,一对一同理）
+    # on_delete = models.SET,  # 删除关联数据,
+    # a.与之关联的值设置为指定值, 设置：models.SET(值)
+    # b.与之关联的值设置为可执行对象的返回值, 设置：models.SET(可执行对象)
+
+
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'blog/static/')
+]

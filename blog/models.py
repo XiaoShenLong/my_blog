6423fd74c1cef8524e5fd31c664df6c9d40fe2f1
hiwoshishei <1374462869@qq.com>
2018-10-15 10:43:51 +0800

    post.views

--- blog/models.py
+++ blog/models.py
@@ -21,17 +21,25 @@ class Post(models.Model):
     body = models.TextField()
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField(auto_now=True)
-    #摘要
+    # 摘要
     excerpt = models.CharField(max_length=200, blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     tags = models.ManyToManyField('Tag', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 博文阅读量
+    views = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.title
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
     class Meta:
         ordering = ['-created_time']
+
+
     # on_delete
     # on_delete = None,  # 删除关联表中的数据时,当前表与其关联的field的行为
     # on_delete = models.CASCADE,  # 删除关联数据,与之关联也删除
